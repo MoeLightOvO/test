86597db349e9f9eb6a287496e49d414a2cd8c2de
MoeLightOvO <yutengxie969@gmail.com>
2022-09-27 15:09:28 +0800

    能不能行？

--- test.java
+++ test.java
@@ -4,11 +4,15 @@ public class test{
         System.out.println(arr[1]);
         test_1();
         printGFInfo();
+        System.out.println(returnBigNum());
     }
     public static void test_1(){
         System.out.println("我测你们码");
     }
     public static void printGFInfo(){
         System.out.println("我不到啊！");
+    }
+    public static int returnBigNum(){
+        return 999;
     }
 }
