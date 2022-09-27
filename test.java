d54b10ae976bd9adea8b935747b5fc018305cf0a
MoeLightOvO <yutengxie969@gmail.com>
2022-09-27 15:06:02 +0800

    为什么只能命令行

--- test.java
+++ test.java
@@ -2,5 +2,13 @@ public class test{
     public static void main(String[] args){
         String []arr={"123","1234"};
         System.out.println(arr[1]);
+        test_1();
+        printGFInfo();
+    }
+    public static void test_1(){
+        System.out.println("我测你们码");
+    }
+    public static void printGFInfo(){
+        System.out.println("我不到啊！");
     }
 }
