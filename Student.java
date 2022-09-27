1f25a6899a3e7f759bccb299c0da3394dca68473
MoeLightOvO <yutengxie969@gmail.com>
2022-09-27 16:55:58 +0800

    瞎鸡儿搞得

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student{
+    public String name;
+    public String sex;
+    public String Id;
+
+    public static void main(String argv[]){
+        Student a=new Student();
+        a.name="羊二车娜姆";
+        a.sex="Not a human";
+        a.Id="114514";
+        System.out.println(a.Id);
+    }
+}
