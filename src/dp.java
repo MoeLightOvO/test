63ce1700f4fa19b8e0e22bc9097fbfe1cb537f44
MoeLightOvO <yutengxie969@gmail.com>
2022-10-09 15:15:31 +0800

    reConstruct

--- src/dp.java
+++ src/dp.java
@@ -3,11 +3,10 @@ public class dp {
     public static void main(String []argv){
         Scanner sc=new Scanner(System.in);
         int k=sc.nextInt();
-        int a,b,c;
-        a=b=c=1;
         int dp[]=new int[k+4];
         dp[0]=1;
         int j=0;
+        sc.close();
         for(int i=1;i<k;){
             if(dp[j]*3>dp[i-(3*j)-1])dp[i++]=dp[j]*3;
             if(dp[j]*5>dp[i-(3*j)-1])dp[i++]=dp[j]*5;
@@ -17,6 +16,6 @@ public class dp {
         for(int i=0;i<k;i++) System.out.println(dp[i]);
 
         //System.out.println(dp[k-1]);
-
+        
     }
 }
