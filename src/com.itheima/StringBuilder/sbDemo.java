c214b15cc190a292567981f4be2cc4dd80db3583
MoeLightOvO <yutengxie969@gmail.com>
2022-10-09 15:34:00 +0800

    test for package 1

--- src/com.itheima/StringBuilder/sbDemo.java
+++ src/com.itheima/StringBuilder/sbDemo.java
@@ -1,2 +1,9 @@
-package com.itheima.StringBuilder
+package com.itheima.StringBuilder;
 import java.util.Scanner;
+public class sbDemo{
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder();
+        sb.append(123);
+        System.out.println(sb);
+    }
+}
